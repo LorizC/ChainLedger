4266bfd01d636287ed04d8a5237f3f1da8597b32
Loriz_Carlos <lorizcarlos16@gmail.com>
2025-09-25 12:24:40 +0800

    Add files via upload

---
+++ html/mainpages/includes/header.php
@@ -0,0 +1,13 @@
+    <!-- Header -->
+    <header>
+      <!-- Burger Icon -->
+      <button id="burger" class="burger-btn">
+        <span class="material-icons-outlined">menu</span>
+      </button>
+
+      <!-- Profile Icons -->
+      <div class="profile">
+        <span class="material-icons-outlined">light_mode</span>
+        <span class="material-icons-outlined">person</span>
+      </div>
+    </header>
---
+++ html/mainpages/includes/sidebar.php
@@ -0,0 +1,31 @@
+<!-- sidebar.php -->
+<aside class="sidebar">
+  <div class="logo">
+    <img src="../../images/logos/logo.png" alt="Logo" class="logo-img">
+  </div>
+  <nav>
+    <a href="dashboard.php" class="">
+      <span class="material-icons-outlined">home</span>
+      <span class="label">Dashboard</span>
+    </a>
+    <a href="ledger.php">
+      <span class="material-icons-outlined">receipt_long</span>
+      <span class="label">Ledger</span>
+    </a>
+    <a href="analytics.php">
+      <span class="material-icons-outlined">bar_chart</span>
+      <span class="label">Analytics</span>
+    </a>
+    <a href="report.php">
+      <span class="material-icons-outlined">description</span>
+      <span class="label">Report</span>
+    </a>
+    <a href="profile.php">
+      <span class="material-icons-outlined">person</span>
+      <span class="label">Profile</span>
+    </a>
+  </nav>
+  <div class="logout">
+    <a href="#"><span class="material-icons-outlined">logout</span></a>
+  </div>
+</aside>
